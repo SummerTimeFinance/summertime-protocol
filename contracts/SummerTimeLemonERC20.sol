08c3eb4bd343f7f40263b38ebdf35fa4080d8a9e
Numbuh Five <summertimedao@protonmail.com>
2021-07-15 15:21:38 +0300

    Added tokenomics into the code

--- contracts/SummerTimeLemonERC20.sol
+++ contracts/SummerTimeLemonERC20.sol
@@ -7,25 +7,47 @@ import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SummerTimeLemonToken is ERC20, ERC20Permit, ERC20Capped, Ownable {
-    // Maximum token cap is 500M
-    uint256 private constant maximumTokenSupply = 500 * 1000000;
-    // Initial amount minted to be used in Presale: 8M
-    uint256 private constant presaleTokenAmount = 8 * 1000000;
+    enum Allocations {
+        MAXIMUM_SUPPLY,
+        FARMING_REWARDS,
+        COMMUNITY_RESERVES,
+        TEAM,
+        PRESALE,
+        ECOSYSTEM_FUND,
+        FOUNDATION
+    }
+
+    uint256 private constant oneMillion = 1000000;
+    uint256 private immutable decimalsPlaces18;
+
+    // SummerTime Finance LEMONS tokenomics
+    mapping (Allocations => uint256) public Tokenomics;
 
     constructor()
         ERC20("SummerTime Lemons", "LEMON")
         ERC20Permit("SummerTime Lemons")
         // Maximum token cap is 500M
-        ERC20Capped(500 * 1000000 * (10 ** uint256(18)))
+        ERC20Capped(500 * oneMillion * 10 ** decimals())
     {
-        _mint(msg.sender, presaleTokenAmount * 10 ** uint256(decimals()));
+        decimalsPlaces18 = 10 ** decimals();
+
+        // Maximum token cap is 500M
+        Tokenomics[Allocations.MAXIMUM_SUPPLY] = 500 * oneMillion;
+        Tokenomics[Allocations.FARMING_REWARDS] = 150 * oneMillion;
+        Tokenomics[Allocations.COMMUNITY_RESERVES] = 150 * oneMillion;
+        Tokenomics[Allocations.TEAM] = 90 * oneMillion;
+        Tokenomics[Allocations.PRESALE] = 30 * oneMillion;
+        Tokenomics[Allocations.ECOSYSTEM_FUND] = 30 * oneMillion;
+        Tokenomics[Allocations.FOUNDATION] = 50 * oneMillion;
+
+        // TypeError: decimalsPlaces18 Immutable variables cannot be read during contract creation time
+        _mint(msg.sender, Tokenomics[Allocations.PRESALE] * 10 ** decimals());
     }
 
     // TIP: Removed "virtual" to disallow any overriding of this function again
     function cap() public view override(ERC20Capped) returns (uint256) {
-        return maximumTokenSupply * (10 ** decimals());
+        return Tokenomics[Allocations.MAXIMUM_SUPPLY] * 10 ** decimals();
     }
-
 
     function _mint(address account, uint256 amount) internal override(ERC20, ERC20Capped) {
         require(ERC20.totalSupply() + amount <= cap(), "LEMON ERC20Capped: cap exceeded");
