bc20b930ce51285c872fec170269aa5f7da81b82
Numbuh Five <summertimedao@protonmail.com>
2021-08-03 13:05:28 +0300

    Fixed issues with Fair LP price oracle

--- contracts/helpers/FairLPPriceOracle.sol
+++ contracts/helpers/FairLPPriceOracle.sol
@@ -1,25 +1,25 @@
 // SPDX-License-Identifier: BSL1.1
 pragma solidity ^0.6.6;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
-import "@uniswap/v2-periphery/contracts/libraries/UniswapV2Library.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+
 import "../libraries/MathUtils.sol";
+import "./LinkPriceOracle.sol";
 
 // @reference https://blog.alphafinance.io/fair-lp-token-pricing/
 // @reference (Converting Fixed Point Values in the Binary Numerical System) https://cutt.ly/cQzTpyn
 contract FairLPPriceOracle is PriceOracle {
-    using SafeMath for uint256;
-    using MathUtils for uint256;
+    // using SafeMath for uint256;
+    // using MathUtils for uint256;
 
-    /// @dev Return the value of the given input as ETH per unit
-    /// @param address The Uniswap token pair to check the value.
-    function getETHPx(address pairAddress)
+    // @dev Return the value of the given input as ETH per unit
+    // @param address The Uniswap token pair to check the value.
+    function getLastLPTokenPrice(address pairAddress)
         external
         view
-        override
         returns (uint256)
     {
         IUniswapV2Pair pairInfo = IUniswapV2Pair(pairAddress);
@@ -27,14 +27,13 @@ contract FairLPPriceOracle is PriceOracle {
         address token1 = pairInfo.token1();
         uint256 totalSupply = pairInfo.totalSupply();
 
-        (uint256 reserveAmount0, uint256 reserveAmount1, ) = lpPair
-            .getReserves();
+        (uint256 reserveAmount0, uint256 reserveAmount1, ) = pairInfo.getReserves();
         uint256 sqrtR = MathUtils.sqrt(
-            SafeMath.mul(reservereserveAmount0, reserveAmount1)
+            SafeMath.mul(reserveAmount0, reserveAmount1)
         );
 
-        uint256 tokenPrice0 = PriceOracle.getLastTokenPrice(token0);
-        uint256 tokenPrice1 = PriceOracle.getLastTokenPrice(token1);
+        uint256 tokenPrice0 = this.getLastTokenPrice(token0);
+        uint256 tokenPrice1 = this.getLastTokenPrice(token1);
         uint256 sqrtP = MathUtils.sqrt(SafeMath.mul(tokenPrice0, tokenPrice1));
 
         return
--- contracts/libraries/MathUtils.sol
+++ contracts/libraries/MathUtils.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.6.6;
+
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
 // computes square roots using the babylonian method
