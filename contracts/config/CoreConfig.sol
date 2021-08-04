0545ef436322f505e3782b4f675241562c7bb1ef
Numbuh Five <summertimedao@protonmail.com>
2021-08-04 16:04:34 +0300

    Add missing defaultInterestRate and reserveAmount

--- contracts/config/CoreConfig.sol
+++ contracts/config/CoreConfig.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: BSL1.1
 pragma solidity ^0.6.6;
 
-contract SummerTimeCoreConfig {
+import "../constants/Defaults.sol";
+
+contract SummerTimeCoreConfig is DefaultConfig {
     // Initial protocol-WIDE DEBT ceiling is: $100,000
     uint256 public summerTimeDebtCeiling = 100000;
 
@@ -12,11 +14,15 @@ contract SummerTimeCoreConfig {
     // Default CCR: 150%
     uint256 public defaultDebtCollateralRatio = 150;
 
+    // Default platform interest rate: 5%
+    uint256 public defaultInterestRate = 5;
+
     // Vault liquidation incentive: 5%
     uint256 public constant liquidationIncentive = 5;
 
     // The portion of accrued interest that goes into reserves, initial set to: 0.10%
     uint256 reserveFactor = 10;
+    uint256 reserveAmount = 0;
 
     // NOTE: Setting to 1000 since there's a very high probabilty we'll never
     // get to 1000 assets available to borrow against, but who knows!!!
@@ -28,8 +34,15 @@ contract SummerTimeCoreConfig {
     // Initial set to: 0.5%
     uint256 public vaultClosingFee = 50;
 
+    // Debt borrowing onetime fee: 0.5%
+    uint256 public debtBorrowingFee = 0;
+
     // For protocol-WIDE pausing depositing or borrowing, incase there is a need to do so
-    // Withdrawals should never be paused.
     bool public protocolDepositingPaused;
     bool public protocolBorrowingPaused;
+
+    function perSecondInterestRate(uint256 givenInterestRate) internal pure returns (uint256) {
+        uint256 everySecondInterestRate = (givenInterestRate * decimal18Places) / secondsInYear;
+        return everySecondInterestRate;
+    }
 }
--- contracts/constants/defaults.sol
+++ contracts/constants/defaults.sol
@@ -3,4 +3,5 @@ pragma solidity ^0.6.6;
 
 contract DefaultConfig {
     uint256 public constant decimal18Places = 10**18;
+    uint256 public constant secondsInYear = 31536000 seconds;
 }
