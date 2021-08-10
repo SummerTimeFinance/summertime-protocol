a3b173e294f5d89fe74760797005feebd3d3285b
Numbuh Five <summertimedao@protonmail.com>
2021-08-10 19:51:20 +0300

    Replaced OpenZepplin ERC20Capped module, _cap variable is not modifiable

--- contracts/tokens/SHELL.sol
+++ contracts/tokens/SHELL.sol
@@ -3,28 +3,28 @@ pragma solidity ^0.6.6;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Capped.sol";
+// import "@openzeppelin/contracts/token/ERC20/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 import "@openzeppelin/contracts/drafts/ERC20Permit.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ShellStableCoin is
     ERC20,
-    ERC20Capped,
     ERC20Burnable,
     ERC20Permit,
     Ownable
 {
+    uint256 internal _cap = 0;
     uint256 internal immutable decimalsPlaces18;
 
-    constructor(uint256 debtCeilingAmount)
+    constructor(uint256 cap_)
         public
         ERC20("SummerTime Shell Stablecoin", "SHELL")
         ERC20Permit("SummerTime Shell Stablecoin")
-        // Maximum token cap is 500M
-        ERC20Capped(debtCeilingAmount * 10**decimals())
     {
         decimalsPlaces18 = 10**decimals();
+        require(cap_ > 0, "ERC20Capped: cap is 0");
+        _cap = cap_;
         // _mint(msg.sender, 0); // nothing to send to the user
     }
 
@@ -44,11 +44,21 @@ contract ShellStableCoin is
         address from,
         address recipient,
         uint256 amount
-    ) internal virtual override(ERC20, ERC20Capped) {
+    ) internal virtual override(ERC20) {
         super._beforeTokenTransfer(from, recipient, amount);
         require(
             recipient != address(this),
             "beforeTransfer: invalid recipient"
         );
+    }
+
+    // @dev Returns the cap on the token's total supply.
+    function cap() public view virtual returns (uint256) {
+        return _cap;
+    }
+
+    function _updateCap(uint256 newCap_) external onlyOwner {
+        require(newCap_ > 0, "ERC20Capped: new cap is 0");
+        _cap = newCap_;
     }
 }
