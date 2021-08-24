ad41a3543a51ebd54675f744b2d593fe5abe0744
Numbuh Five <summertimedao@protonmail.com>
2021-08-24 15:54:43 +0300

    Add ALL SafeMath methods into MathUtil to use it as a replacement of SafeMaths

--- contracts/libraries/MathUtils.sol
+++ contracts/libraries/MathUtils.sol
@@ -7,12 +7,43 @@ pragma solidity ^0.6.6;
 // computes square roots using the babylonian method
 // https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method
 library MathUtils {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a, "SafeMath: subtraction overflow");
+        return a - b;
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) return 0;
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+        return c;
+    }
+
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
         require(b > 0, "MathUtils: division by zero");
         return a / b;
     }
 
-    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+    function sub(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        return a - b;
+    }
+
+    function div(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
         require(b > 0, errorMessage);
         return a / b;
     }
